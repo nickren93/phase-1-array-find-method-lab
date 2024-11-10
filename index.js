0158862cbe0f890179c9aa3cd8391f1a3fbbd641
Sitong <nickren93@gmail.com>
2024-11-10 14:59:42 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function superbowlWin(array){
+    function win(element){
+        return element.result === "W"
+    }if (array.find(win) === undefined) {
+        return undefined
+    }else {
+        return array.find(win).year
+    }
+    
+}
+
+console.log(superbowlWin(record))
+
